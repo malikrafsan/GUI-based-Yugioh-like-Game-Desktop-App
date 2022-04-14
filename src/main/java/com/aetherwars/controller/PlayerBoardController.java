f92de1a0e179ce03a578a7a84ec7a7706e512fca
Malik Rafsanjani <13520105@std.stei.itb.ac.id>
2022-04-14 17:15:30 +0700

    feat(view): add mouse event handlers on active cards and player icon

--- src/main/java/com/aetherwars/controller/PlayerBoardController.java
+++ src/main/java/com/aetherwars/controller/PlayerBoardController.java
@@ -1,9 +1,11 @@
 package com.aetherwars.controller;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
@@ -15,6 +17,7 @@ import com.gluonhq.charm.glisten.control.ProgressBar;
 
 
 public class PlayerBoardController {
+    @FXML private AnchorPane playerBoardContainer;
     @FXML private ImageView playerImageView;
     @FXML private ImageView activeCard1;
     @FXML private ImageView activeCard2;
@@ -24,9 +27,13 @@ public class PlayerBoardController {
     @FXML private ImageView[] activeCards;
     @FXML private ProgressBar progressBar;
 
+    private int ID_BOARD;
     private final String IMG_DIR_PATH = "/com/aetherwars/card/image/character/";
 
     @FXML private void initialize() {
+        this.ID_BOARD = Integer.parseInt(this.playerBoardContainer.getId());
+        System.out.println("ID_BOARD: " + this.ID_BOARD);
+
         this.activeCards = new ImageView[] {activeCard1, activeCard2, activeCard3, activeCard4, activeCard5};
         String playerImgPath = "Villager.png";
         String[] cardImgPaths = new String[] {"Zombie.png", null, "Skeleton.png", null, "Ghast.png"};
@@ -35,6 +42,19 @@ public class PlayerBoardController {
             File file = new File(getClass().getResource(this.IMG_DIR_PATH + playerImgPath).toURI());
             Image playerImg = new Image(file.toURI().toString());
             this.playerImageView.setImage(playerImg);
+            int boardID = this.ID_BOARD;
+            this.playerImageView.setOnMouseEntered(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    System.out.println("PLAYER " + boardID + " ICON MOUSE ENTER EVENT DETECTED");
+                }
+            });
+            this.playerImageView.setOnMouseExited(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    System.out.println("PLAYER ICON MOUSE EXIT EVENT DETECTED");
+                }
+            });
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -45,6 +65,29 @@ public class PlayerBoardController {
                     File file = new File(getClass().getResource(this.IMG_DIR_PATH + cardImgPaths[i]).toURI());
                     Image activeCardImg = new Image(file.toURI().toString());
                     this.activeCards[i].setImage(activeCardImg);
+                    int finalI = i;
+                    int boardId = this.ID_BOARD;
+
+                    this.activeCards[i].setOnMouseClicked(new EventHandler<MouseEvent>() {
+                        @Override
+                        public void handle(MouseEvent event) {
+                            System.out.println("CARD " + (finalI + 1) + " FROM PLAYER " + boardId + " IS CLICKED");
+                        }
+                    });
+
+                    this.activeCards[i].setOnMouseEntered(new EventHandler<MouseEvent>() {
+                        @Override
+                        public void handle(MouseEvent event) {
+                            System.out.println("CARD " + (finalI + 1) + " FROM PLAYER " + boardId + " IS HOVERED");
+                        }
+                    });
+
+                    this.activeCards[i].setOnMouseExited(new EventHandler<MouseEvent>() {
+                        @Override
+                        public void handle(MouseEvent event) {
+                            System.out.println("CARD " + (finalI + 1) + " NO LONGER HOVERED");
+                        }
+                    });
                 }
             } catch (Exception e) {
                 System.out.println(e);
