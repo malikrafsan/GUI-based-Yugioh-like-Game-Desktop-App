6c8aca0298f102babbd3c6cf95372637ecf2ccd9
Malik Rafsanjani <13520105@std.stei.itb.ac.id>
2022-04-14 15:20:23 +0700

    fix(main): resolve error on import class in loadCard method

--- src/main/java/com/aetherwars/AetherWars.java
+++ src/main/java/com/aetherwars/AetherWars.java
@@ -13,32 +13,16 @@ import javafx.scene.Scene;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import com.aetherwars.model.Type;
-import com.aetherwars.model.Character;
 import com.aetherwars.util.CSVReader;
 
 public class AetherWars extends Application {
   private static final String CHARACTER_CSV_FILE_PATH = "card/data/character.csv";
-
-  public void loadCards() throws IOException, URISyntaxException {
-    File characterCSVFile = new File(getClass().getResource(CHARACTER_CSV_FILE_PATH).toURI());
-    CSVReader characterReader = new CSVReader(characterCSVFile, "\t");
-    characterReader.setSkipHeader(true);
-    List<String[]> characterRows = characterReader.read();
-    for (String[] row : characterRows) {
-      Character c = new Character(row[1], row[3], Type.valueOf(row[2]));
-      System.out.println(c);
-    }
-  }
 
   @Override
   public void start(Stage stage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("/view/App.fxml"));
     stage.setScene(new Scene(root));
     stage.show();
-
-    this.loadCards();
-
   }
 
   public static void main(String[] args) {
