f8be6b8bfb31261ccca0b074a2485f4a870417f5
Malik Rafsanjani <13520105@std.stei.itb.ac.id>
2022-04-14 16:20:45 +0700

    feat(view): add TODO for lower container

--- src/main/java/com/aetherwars/controller/AppController.java
+++ src/main/java/com/aetherwars/controller/AppController.java
@@ -14,19 +14,19 @@ public class AppController {
     @FXML private AnchorPane player1Board;
     @FXML private AnchorPane turnContainer;
     @FXML private AnchorPane player2Board;
-    @FXML private HBox container;
+    @FXML private HBox upperContainer;
 
     @FXML private void initialize() throws Exception {
         FXMLLoader player1BoardLoader = new FXMLLoader(getClass().getResource("/view/Player1Board.fxml"));
         this.player1Board = player1BoardLoader.load();
-        this.container.getChildren().add(this.player1Board);
+        this.upperContainer.getChildren().add(this.player1Board);
 
         FXMLLoader turnContainerLoader = new FXMLLoader(getClass().getResource("/view/TurnContainer.fxml"));
         this.turnContainer = turnContainerLoader.load();
-        this.container.getChildren().add(this.turnContainer);
+        this.upperContainer.getChildren().add(this.turnContainer);
 
         FXMLLoader player2BoardLoader = new FXMLLoader(getClass().getResource("/view/Player2Board.fxml"));
         this.player2Board = player2BoardLoader.load();
-        this.container.getChildren().add(this.player2Board);
+        this.upperContainer.getChildren().add(this.player2Board);
     }
 }
