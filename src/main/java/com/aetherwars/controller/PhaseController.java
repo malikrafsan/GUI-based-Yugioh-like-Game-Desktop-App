a043823d90a9dc565e996eb33a395831eb011179
malikrafsan <pro.malikakbar2357@gmail.com>
2022-04-28 21:00:11 +0700

    feat(view): integrate phase GUI with game manager

--- src/main/java/com/aetherwars/controller/PhaseController.java
+++ src/main/java/com/aetherwars/controller/PhaseController.java
@@ -7,8 +7,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 import java.util.Observable;
+import java.util.Observer;
 
-public class PhaseController {
+public class PhaseController implements Observer {
     @FXML private Label labelDraw;
     @FXML private Label labelPlan;
     @FXML private Label labelAttack;
@@ -19,17 +20,14 @@ public class PhaseController {
     private final String inactivePhase = "-fx-background-color: rgb(240,236,236); -fx-border-color: black;";
     private final String activePhase = "-fx-background-color: orange; -fx-border-color: black;";
 
-//    private Phase currentPhase;
-
-
     @FXML
     public void initialize(){
         labels[0] = labelDraw;
         labels[1] = labelPlan;
         labels[2] = labelAttack;
         labels[3] = labelEnd;
-        //currentPhase = Phase.DRAW;
-        setActiveLabel(labelDraw);
+        // setActiveLabel(labelDraw);
+        GameManager.getInstance().addObserver("GAMESTATE", this);
     }
 
     private void setActiveLabel(Label labelToSet){
@@ -43,66 +41,26 @@ public class PhaseController {
         }
     }
 
-//    public void setNextPhase() {
-//        switch (currentPhase) {
-//            case DRAW:
-//                setActiveLabel(labelPlan);
-//                currentPhase = Phase.PLANNING;
-//                break;
-//            case PLANNING:
-//                setActiveLabel(labelAttack);
-//                currentPhase = Phase.ATTACK;
-//                break;
-//            case ATTACK:
-//                setActiveLabel(labelEnd);
-//                currentPhase = Phase.END;
-//                break;
-//            case END:
-//                setActiveLabel(labelDraw);
-//                currentPhase = Phase.DRAW;
-//
-//                //testing
-//                appController.drawPhase();
-//
-//                break;
-//        }
-//    }
-
     public void setPhase(Phase phase) {
         switch (phase) {
             case DRAW:
                 setActiveLabel(labelDraw);
-//                currentPhase = Phase.PLANNING;
                 break;
             case PLANNING:
                 setActiveLabel(labelPlan);
-//                currentPhase = Phase.ATTACK;
                 break;
             case ATTACK:
                 setActiveLabel(labelAttack);
-//                currentPhase = Phase.END;
                 break;
             case END:
                 setActiveLabel(labelEnd);
-//                currentPhase = Phase.DRAW;
-
-                //testing
-//                appController.drawPhase();
-                // TODO : use game manager
                 break;
         }
     }
 
-
-
-//    public void setAppController(AppController appController){
-//        this.appController = appController;
-//    }
-
     @FXML
     private void nextPhase(ActionEvent e){
-        // TODO: call game manager
-        setPhase(Phase.ATTACK);
+        GameManager.getInstance().nextPhase();
     }
 
     public void update(Observable obs, Object obj) {
