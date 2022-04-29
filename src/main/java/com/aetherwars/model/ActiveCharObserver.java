5f989c8a5eb7a290378b32444f7db7b789f1aa03
hafidznrg <hafidznur.hn@gmail.com>
2022-04-29 20:38:13 +0700

    add unit test active char observer

--- src/main/java/com/aetherwars/model/ActiveCharObserver.java
+++ src/main/java/com/aetherwars/model/ActiveCharObserver.java
@@ -26,7 +26,7 @@ public class ActiveCharObserver extends Observable implements IActiveCharObserve
     }
 
     public void addChar(CharacterCard card, int index) {
-        System.out.println("\n\nadd char to board\n\n");
+        // System.out.println("\n\nadd char to board\n\n");
         this.chars[index] = new ActiveChar(card);
         this.sync();
     }
