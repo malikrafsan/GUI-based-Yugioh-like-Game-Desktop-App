43a8f321b4c5844420aee0843d636fc3ec27d385
Enderageous <72645928+Enderageous@users.noreply.github.com>
2022-04-29 21:24:42 +0700

    Update PlayerTest.java

--- src/main/java/com/aetherwars/Test/PlayerTest.java
+++ src/main/java/com/aetherwars/Test/PlayerTest.java
@@ -1,16 +1,32 @@
 package com.aetherwars.Test;
 
 import com.aetherwars.model.*;
+import com.aetherwars.controller.GameManager;
 import org.junit.jupiter.api.Test;
+
+import java.awt.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
     Player steve = new Player();
 
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // DICOBA SATU SATU KARENA KELAS Player MENGGUNAKAN static
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    @Test
+    void getMaxMana() {
+        assertEquals(steve.getMaxMana(), 1);
+    }
+
+    @Test
+    void getMana() {
+        assertEquals(steve.getMana(), 1);
+    }
+
     @Test
     void useMana() {
-        assertEquals(steve.getMana(), 1);
         steve.useMana(1);
         assertEquals(steve.getMana(), 0);
     }
@@ -22,7 +38,7 @@ class PlayerTest {
 
     @Test
     void minusHealth() {
-        steve.minusHealth(5);
-        assertEquals(steve.getHealth(), 75);
+        steve.minusHealth(10);
+        assertEquals(steve.getHealth(), 70);
     }
 }
