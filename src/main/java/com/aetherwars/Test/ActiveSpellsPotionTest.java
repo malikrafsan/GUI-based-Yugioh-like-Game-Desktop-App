32ee8e7960783c6d716e04296f9339ce6c04291c
Enderageous <72645928+Enderageous@users.noreply.github.com>
2022-04-29 16:48:02 +0700

    bug

---
+++ src/main/java/com/aetherwars/Test/ActiveSpellsPotionTest.java
@@ -0,0 +1,40 @@
+package com.aetherwars.Test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActiveSpellsPotionTest {
+
+    @Test
+    void swap() {
+    }
+
+    @Test
+    void healAll() {
+    }
+
+    @Test
+    void getAttackPotion() {
+    }
+
+    @Test
+    void getHealthPotion() {
+    }
+
+    @Test
+    void getDuration() {
+    }
+
+    @Test
+    void receiveDamage() {
+    }
+
+    @Test
+    void newRound() {
+    }
+
+    @Test
+    void display() {
+    }
+}
